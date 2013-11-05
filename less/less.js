237397b0d001928036f0df791f892519c4f87779
petersirka <petersirka@gmail.com>
2013-11-05 20:39:32 +0100

    new module

---
+++ less/less.js
@@ -0,0 +1,53 @@
+var less = require('less');
+var fs = require('fs');
+
+exports.install = function(framework) {
+	// THE PROBLEM:
+	// LESS CSS does not support synchronous compiler
+	// We must create a file route
+	framework.file('LESS CSS', function(req) { return req.url.indexOf('.css') !== -1 || req.url.indexOf('.less') !== -1; }, less_compiler);
+
+	var accept = framework.config['static-accepts'];
+
+	if (accept.indexOf('less') === -1)
+		accept.push('less');
+};
+
+function less_compiler(req, res) {
+
+	// this === framework
+	var self = this;
+
+	// create temporary filename
+	// we'll compile file
+	var filename = self.path.temp(req.url.replace(/\//g, '-')).substring(1);
+
+	// Cache for RELEASE MODE ONLY
+	if (framework.isProcessed(filename)) {
+		self.responseFile(req, res, filename);
+		return;
+	}
+
+	fs.readFile(self.path.public(req.url), function(err, data) {
+
+		if (err) {
+			self.response404(req, res);
+			return;
+		}
+
+		less.render(data.toString('utf8'), function(err, css) {
+
+			if (err) {
+				self.response500(req, res, err);
+				return;
+			}
+
+			// write compiled content into the temporary file
+			fs.writeFileSync(filename, css);
+
+			// this function affect framework.isProcessed() function
+			self.responseFile(req, res, filename);
+		});
+
+	});
+}
---
+++ less/readme.md
@@ -0,0 +1,11 @@
+# LESS CSS module
+
+- partial.js version +v1.2.8-0
+- install npm less
+- copy **less.js** to __/your-partialjs-website/modules/__
+
+All files with .CSS and .LESS extension will compiled via LESS. In release mode will compiled files cached in temporary directory.
+
+## IMPORTANT
+
+This module does not support compiling of dynamic CSS - supports only compiling of files.
