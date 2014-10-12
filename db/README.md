6af1dad979a62ca16cc6f16975517f1b9c054c02
KiPSOFT <newkipsoftware@gmail.com>
2014-10-13 01:47:26 +0300

    added 10 function and update documentation.

--- db/README.md
+++ db/README.md
@@ -16,31 +16,341 @@ In total.js configuration file;
     db-password        : masterkey
 ```
 
-## Functions
+## Supported Functions
 
-- init
-- get
-- from
-- join
-- select_min
-- select_sum
-- select_avg
-- select_max
-- get_where
-- or_where_not_in
-- where_not_in
-- or_where_in
-- where_in
-- or_where
-- or_not_like
-- not_like
-- or_like
-- like
-- where
-- group_by
-- select
-- query
-- load
+Global
+---
+
+
+
+set - set field values.(fieldName, value) 
+-----------------------------
+**Parameters**
+
+**fieldName**: string | object, single field name or fields and values in json object.
+
+**value**: *, field value.
+
+
+delete - delete record in database.(tableName, data, cb) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, delete table name.
+
+**data**: object, delete where object.
+
+**cb**: function, callback function. return one argument. err handling.
+
+
+update - update selected record.(tableName, data, cb) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, update table name.
+
+**data**: object, update data object. include fiels and values. optional.
+
+**cb**: function, callback function. return one argument. err handling.
+
+
+insert - insert record to database.(tableName, data, cb) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, insert table name.
+
+**data**: object, fields and values json object. optional.
+
+**cb**: function, callback function. return one argument. err handling.
+
+
+load - this function load and connect db.(cb) 
+-----------------------------
+**Parameters**
+
+**cb**: function, callback this return err handling.
+
+
+query - this function execute sql on database(sql, params, cb) 
+-----------------------------
+**Parameters**
+
+**sql**: string, 
+
+**params**: string | Array.&lt;string&gt;, 
+
+**cb**: function, callback have two params. result and err.
+
+
+select - set select string to sql.(sel) 
+-----------------------------
+**Parameters**
+
+**sel**: string, select string. required.
+
+
+distinct - add distinct keyword to a query.() 
+-----------------------------
+
+
+having - add having.(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string | object, having field name or object.
+
+**value**: string, having value.
+
+
+or_having - onyl seperates multiple clauses with or(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string | object, having field name or object.
+
+**value**: string, having value.
+
+
+group_by - add group by clauses to a query.(param) 
+-----------------------------
+**Parameters**
+
+**param**: string, group field name.
+
+
+order_by - set a order by clause.(param, type) 
+-----------------------------
+**Parameters**
+
+**param**: string, fields names.
+
+**type**: string, order by sorting type. default asc
+
+
+where - this function create where string in sql(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string | Array.&lt;string&gt;, key param or json object.
+
+**value**: string, value optional param.
+
+
+like - add like clauses in sql.(param, value, type) 
+-----------------------------
+**Parameters**
+
+**param**: string, field name
+
+**value**: string, field value.
+
+**type**: string, like clauses type. before, after or both. default both.
+
+
+or_like - add like clauses in sql.(param, value, type) 
+-----------------------------
+**Parameters**
+
+**param**: string, field name
+
+**value**: string, field value.
+
+**type**: string, like clauses type. before, after or both. default both.
+
+
+not_like - add not like clauses in sql.(param, value, type) 
+-----------------------------
+**Parameters**
+
+**param**: string, field name
+
+**value**: string, field value.
+
+**type**: string, like clauses type. before, after or both. default both.
+
+
+or_not_like - add not like clauses in sql.(param, value, type) 
+-----------------------------
+**Parameters**
+
+**param**: string, field name
+
+**value**: string, field value.
+
+**type**: string, like clauses type. before, after or both. default both.
+
+
+or_where - add or clauses.(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string | Array.&lt;string&gt;, key param or json object.
+
+**value**: string, value optional param.
+
+
+where_in - add &quot;in&quot; clauses.(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string, in field name.
+
+**value**: Array.&lt;string&gt;, in values. string array.
+
+
+or_where_in - add &quot;in&quot; clauses.(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string, in field name.
+
+**value**: Array.&lt;string&gt;, in values. string array.
+
+
+where_not_in - add &quot;not in&quot; clauses.(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string, in field name.
+
+**value**: Array.&lt;string&gt;, in values. string array.
+
+
+or_where_not_in - add &quot;not in&quot; clauses.(param, value) 
+-----------------------------
+**Parameters**
+
+**param**: string, in field name.
+
+**value**: Array.&lt;string&gt;, in values. string array.
+
+
+where - this function create where string in sql(self, param, value, type, operator) 
+-----------------------------
+**Parameters**
+
+**self**: object, db class instance.
+
+**param**: string | Array.&lt;string&gt;, key param
+
+**value**: string, value optional param.
+
+**type**: string, and, or. optional param. default value and.
+
+**operator**: string, optional. default value =.
+
+
+get_where - get and where sql.(tableName, where, limit, offset, cb) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, get table name.
+
+**where**: object, where sql. accept only json object.
+
+**limit**: number, limit size. optional.
+
+**offset**: number, offset position. optional.
+
+**cb**: function, callback have a two params. result and err handling.
+
+
+select_max - this function add max aggregate in select string.(field, alias) 
+-----------------------------
+**Parameters**
+
+**field**: string, max field name.
+
+**alias**: string, max field alias name.
+
+
+select_avg - this function add avg aggregate in select string.(field, alias) 
+-----------------------------
+**Parameters**
+
+**field**: string, field name.
+
+**alias**: string, field alias name.
+
+
+select_sum - this function add sum aggregate in select string.(field, alias) 
+-----------------------------
+**Parameters**
+
+**field**: string, field name.
+
+**alias**: string, field alias name.
+
+
+select_min(field, alias) 
+-----------------------------
+**Parameters**
+
+**field**: string, 
+
+**alias**: string, 
+
+
+join - add sql join in sql string.(tableName, condition, joinType) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, join table name.
+
+**condition**: string, join on condition
+
+**joinType**: string, join type. left, right. default value is join.
+
+
+from - add table name in sql(tableName) 
+-----------------------------
+**Parameters**
+
+**tableName**: required.
+
+
+count_all_results - get query row count.(tableName, cb) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, count table name.
+
+**cb**: function, callback function. have a two arguments. count and error.
+
+
+count_all - get row count from table name.(tableName, cb) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, 
+
+**cb**: function, have a two arguments. count and error.
+
+
+limit -(limit, offset) 
+-----------------------------
+**Parameters**
+
+**limit**: number, limit value.
+
+**offset**: number, offset position. optional value.
+
+
+get - get rows from table.(tableName, limit, offset, cb) 
+-----------------------------
+**Parameters**
+
+**tableName**: string, table name.
+
+**limit**: number, limit size. (optional)
+
+**offset**: number, offset position. (optional)
+
+**cb**: function, callback have a two params. result and err handling.
+
+
+---
 
 ## Examples
 
--- db/v1.00/db.js
+++ db/v1.00/db.js
@@ -3,7 +3,7 @@
 exports.name = 'db';
 exports.version = '1.00';
 
-/***
+/**
  * db class.
  * @class
  * @private
@@ -13,14 +13,121 @@ function _db() {
     this.options = {};
     this.driverName = null;
     this.database = null;
-    this.selectstr = "*";
-    this.wherestr = "";
-    this.fromstr = "";
-    this.joinstr = " ";
-    this.groupbystr = "";
+    clearValues(this);
 }
 
-/***
+
+function clearValues(self) {
+    self.selectstr = "*";
+    self.wherestr = "";
+    self.fromstr = "";
+    self.joinstr = " ";
+    self.groupbystr = "";
+    self.distinctstr = "";
+    self.havingstr = "";
+    self.orderbystr = "";
+    self.limitstr = "";
+    self.fields = [];
+    self.values = [];
+};
+
+/**
+ * @function set - set field values.
+ * @param {string|object} fieldName - single field name or fields and values in json object.
+ * @param {*} [value] - field value.
+ */
+_db.prototype.set = function (fieldName, value) {
+    var self = this;
+    if (typeof fieldName !== "object" && value !== undefined) {
+        self.fields.push(fieldName);
+        if (typeof value === "string")
+            self.values.push("'" + value + "'");
+        else
+            self.values.push(value);
+    }
+    else if (typeof fieldName === "object" && value === undefined) {
+        for (var key in fieldName) {
+            self.fields.push(key);
+            if (typeof fieldName[key] === "string")
+                self.values.push("'" + fieldName[key] + "'");
+            else
+                self.values.push(fieldName[key]);
+        }
+    }
+};
+
+/**
+ * @function delete - delete record in database.
+ * @param {string} tableName - delete table name.
+ * @param {object} [data] - delete where object.
+ * @param {function} cb - callback function. return one argument. err handling.
+ */
+_db.prototype.delete = function(tableName, data, cb) {
+    var self = this;
+    if (typeof data !== "function" && typeof data === "object") {
+        self.where(data);
+    }
+    var sql = "DELETE FROM "+tableName+" "+self.wherestr;
+    self.query(sql, undefined, function (result, err) {
+        if (typeof data === "function")
+            data(err);
+        else
+            cb(err);
+        clearValues(self);
+    });
+};
+
+
+/**
+ * @function update - update selected record.
+ * @param {string} tableName - update table name.
+ * @param {object} [data] - update data object. include fiels and values. optional.
+ * @param {function} cb - callback function. return one argument. err handling.
+ */
+_db.prototype.update = function(tableName, data, cb) {
+    var self = this;
+    if (typeof data !== "function" && typeof data === "object") {
+        self.set(data);
+    }
+    var tmp = "";
+
+    self.fields.forEach(function(element, index) {
+        if (tmp !== "")
+            tmp +=", ";
+        tmp += element + " = "+self.values[index];
+    });
+    var sql = "UPDATE " + tableName + " SET " + tmp + " " + self.wherestr;
+    self.query(sql, undefined, function (result, err) {
+        if (typeof data === "function")
+            data(err);
+        else
+            cb(err);
+        clearValues(self);
+    });
+};
+
+/**
+ * @function insert - insert record to database.
+ * @param {string} tableName - insert table name.
+ * @param {object} [data] - fields and values json object. optional.
+ * @param {function} cb - callback function. return one argument. err handling.
+ */
+_db.prototype.insert = function (tableName, data, cb) {
+    var self = this;
+    if (typeof data !== "function" && typeof data === "object") {
+        self.set(data);
+    }
+    var sql = "INSERT INTO " + tableName + " (" + self.fields.join() + ") VALUES (" + self.values.join() + ")";
+    self.query(sql, undefined, function (result, err) {
+        if (typeof data === "function")
+            data(err);
+        else
+            cb(err);
+        clearValues(self);
+    });
+};
+
+/**
  * @function load - this function load and connect db.
  * @param {function} cb - callback this return err handling.
  */
@@ -48,10 +155,10 @@ _db.prototype.load = function (cb) {
     }
 };
 
-/***
+/**
  * @function query - this function execute sql on database
  * @param {string} sql
- * @param {string|[]} params
+ * @param {string|string[]} params
  * @param {function} cb - callback have two params. result and err.
  */
 _db.prototype.query = function (sql, params, cb) {
@@ -72,7 +179,7 @@ _db.prototype.query = function (sql, params, cb) {
     }
 };
 
-/***
+/**
  * @function select - set select string to sql.
  * @param {string} sel - select string. required.
  */
@@ -84,18 +191,109 @@ _db.prototype.select = function (sel) {
         self.selectstr += "," + sel;
 };
 
-_db.prototype.group_by = function(param) {
+/**
+ * @function distinct - add distinct keyword to a query.
+ */
+_db.prototype.distinct = function () {
+    var self = this;
+    self.distinctstr = "distinct";
+};
+
+/**
+ * @function _having
+ * @param {object} self - db instance.
+ * @param {string|object} param - having fields name or object.
+ * @param {string} [value] - having value.
+ * @param {string} [type] - default type ","
+ * @param {string} [operator] - default operator =
+ */
+function _having(self, param, value, type, operator) {
+    if (operator === undefined) operator = "=";
+    if (type === undefined) type = ",";
+
+    function checkVal(val) {
+        if (typeof val === "string")
+            return "'" + val + "'";
+        else
+            return val;
+    }
+
+    function checkOperator(key) {
+        if (key.indexOf(">") > 0 || key.indexOf(">=") > 0 || key.indexOf("<=") > 0 || key.indexOf("!=") > 0 || key.indexOf("=") > 0 || key.indexOf("in") > 0 || key.indexOf("like") > 0)
+            return " ";
+        else return " " + operator + " ";
+    }
+
+    if (typeof param === "object" && value === undefined) {
+        self.havingstr = "having ";
+        param.forEach(function (element, index) {
+            if (self.havingstr !== "having ")
+                self.havingstr += type;
+            self.havingstr += element["field"] + checkOperator(element["field"]) + checkVal(element["value"]);
+        });
+    }
+    else if (typeof param === "string" && value !== undefined) {
+        self.havingstr = "having " + param + checkOperator(param) + checkVal(value);
+    }
+    self.havingstr += " ";
+};
+
+/**
+ * @function having - add having.
+ * @param {string|object} param - having field name or object.
+ * @param {string} [value] - having value.
+ */
+_db.prototype.having = function (param, value) {
+    var self = this;
+    _having(self, param, value);
+};
+
+/**
+ * @function or_having - onyl seperates multiple clauses with or
+ * @param {string|object} param - having field name or object.
+ * @param {string} [value] - having value.
+ */
+_db.prototype.or_having = function (param, value) {
+    var self = this;
+    _having(self, param, value, " or ");
+};
+
+/**
+ * @function group_by - add group by clauses to a query.
+ * @param {string} param - group field name.
+ */
+_db.prototype.group_by = function (param) {
     var self = this;
     var tmp = "";
     if (typeof param === "array") tmp = param.join();
     else tmp = param;
-    self.groupbystr = " group by "+tmp;
+    self.groupbystr = " group by " + tmp + " ";
 };
 
+/**
+ * @function order_by - set a order by clause.
+ * @param {string} param - fields names.
+ * @param {string} [type] - order by sorting type. default asc
+ */
+_db.prototype.order_by = function (param, type) {
+    var self = this;
+    if (type === undefined) type = "asc";
+    if (typeof param === "string" && type !== undefined) {
+        if (self.orderbystr === "")
+            self.orderbystr = "order by";
+        else
+            self.orderbystr += ",";
+        self.orderbystr += " " + param + " " + type;
+    }
+    if (typeof param === "string" && type === undefined) {
+        self.orderbystr = "order by " + param + " " + type;
+    }
+    self.orderbystr += " ";
+};
 
-/***
+/**
  * @function where - this function create where string in sql
- * @param {string|[]} param - key param or json object.
+ * @param {string|string[]} param - key param or json object.
  * @param {string} [value] - value optional param.
  */
 _db.prototype.where = function (param, value) {
@@ -103,7 +301,7 @@ _db.prototype.where = function (param, value) {
     _where(self, param, value, "and");
 };
 
-/***
+/**
  * @function like - add like clauses in sql.
  * @param {string} param - field name
  * @param {string} value - field value.
@@ -118,7 +316,7 @@ _db.prototype.like = function (param, value, type) {
     _where(self, param, value, "and", "like");
 };
 
-/***
+/**
  * @function or_like - add like clauses in sql.
  * @param {string} param - field name
  * @param {string} value - field value.
@@ -133,7 +331,7 @@ _db.prototype.or_like = function (param, value, type) {
     _where(self, param, value, "or", "like");
 };
 
-/***
+/**
  * @function not_like - add not like clauses in sql.
  * @param {string} param - field name
  * @param {string} value - field value.
@@ -148,7 +346,7 @@ _db.prototype.not_like = function (param, value, type) {
     _where(self, param, value, "and", "not like");
 };
 
-/***
+/**
  * @function or_not_like - add not like clauses in sql.
  * @param {string} param - field name
  * @param {string} value - field value.
@@ -163,9 +361,9 @@ _db.prototype.or_not_like = function (param, value, type) {
     _where(self, param, value, "or", "not like");
 };
 
-/***
+/**
  * @function or_where - add or clauses.
- * @param {string|[]} param - key param or json object.
+ * @param {string|string[]} param - key param or json object.
  * @param {string} [value] - value optional param.
  */
 _db.prototype.or_where = function (param, value) {
@@ -173,7 +371,7 @@ _db.prototype.or_where = function (param, value) {
     _where(self, param, value, "or");
 };
 
-/***
+/**
  * @function where_in - add "in" clauses.
  * @param {string} param - in field name.
  * @param {string[]} value - in values. string array.
@@ -191,7 +389,7 @@ _db.prototype.where_in = function (param, value) {
     _where(self, param, "(" + tmp + ")", "in");
 };
 
-/***
+/**
  * @function or_where_in - add "in" clauses.
  * @param {string} param - in field name.
  * @param {string[]} value - in values. string array.
@@ -209,7 +407,7 @@ _db.prototype.or_where_in = function (param, value) {
     _where(self, param, "(" + tmp + ")", "or", "in");
 };
 
-/***
+/**
  * @function where_not_in - add "not in" clauses.
  * @param {string} param - in field name.
  * @param {string[]} value - in values. string array.
@@ -227,7 +425,7 @@ _db.prototype.where_not_in = function (param, value) {
     _where(self, param, "(" + tmp + ")", "not in");
 };
 
-/***
+/**
  * @function or_where_not_in - add "not in" clauses.
  * @param {string} param - in field name.
  * @param {string[]} value - in values. string array.
@@ -245,11 +443,11 @@ _db.prototype.or_where_not_in = function (param, value) {
     _where(self, param, "(" + tmp + ")", "or", "not in");
 };
 
-/***
+/**
  * @function where - this function create where string in sql
  * @private
  * @param {object} self - db class instance.
- * @param {string|[]} param - key param
+ * @param {string|string[]} param - key param
  * @param {string} [value] - value optional param.
  * @param {string} [type] - and, or. optional param. default value and.
  * @param {string} [operator] - optional. default value =.
@@ -283,10 +481,11 @@ function _where(self, param, value, type, operator) {
     if (self.wherestr === "") self.wherestr = "where ";
 
     if (typeof param === "object" && value === undefined) {
-        for (var key in param) {
-            var val = param[key];
-            self.wherestr += checkWhere() + checkOperator(key) + operator + checkVal(val);
-        }
+        var val;
+        param.forEach(function (element) {
+            val = element["value"];
+            self.wherestr += checkWhere() + element["field"] + checkOperator(element["field"]) + checkVal(val);
+        });
     }
     else if (typeof param === "string" && value === undefined)
         self.wherestr += param;
@@ -296,7 +495,7 @@ function _where(self, param, value, type, operator) {
     }
 };
 
-/***
+/**
  * @function get_where - get and where sql.
  * @param {string} tableName - get table name.
  * @param {object} where - where sql. accept only json object.
@@ -310,7 +509,7 @@ _db.prototype.get_where = function (tableName, where, limit, offset, cb) {
     self.get(tableName, limit, offset, cb);
 };
 
-/***
+/**
  * @function select_max - this function add max aggregate in select string.
  * @param {string} field - max field name.
  * @param {string} [alias] - max field alias name.
@@ -343,7 +542,7 @@ _db.prototype.select_sum = function (field, alias) {
     self.select("SUM(" + field + ") as " + alias);
 };
 
-/***
+/**
  * @funtion select_min - this function add min aggregate in select string.
  * @param {string} field
  * @param {string} [alias]
@@ -355,7 +554,7 @@ _db.prototype.select_min = function (field, alias) {
 };
 
 
-/***
+/**
  * @function join - add sql join in sql string.
  * @param {string} tableName - join table name.
  * @param {string} condition - join on condition
@@ -381,7 +580,63 @@ _db.prototype.from = function (tableName) {
     self.fromstr = tableName;
 };
 
-/***
+/**
+ * @function count_all_results - get query row count.
+ * @param {string} [tableName] - count table name.
+ * @param {function} cb - callback function. have a two arguments. count and error.
+ */
+_db.prototype.count_all_results = function (tableName, cb) {
+    var self = this;
+    self.selectstr = "count(*) as top";
+    self.limitstr = "";
+    self.distinctstr = "";
+    if (typeof tableName !== "function")
+        self.fromstr = tableName;
+    self.orderbystr = "";
+    self.get(function (result, err) {
+        if (typeof tableName === "function")
+            if (err)
+                tableName(0, err);
+            else
+                tableName(result[0].top, err);
+        else if (err)
+            cb(0, err);
+        else
+            cb(result[0].top, err);
+    });
+};
+
+/**
+ * @function count_all - get row count from table name.
+ * @param {string} tableName
+ * @param {function} cb - have a two arguments. count and error.
+ */
+_db.prototype.count_all = function (tableName, cb) {
+    var self = this;
+    clearValues(self);
+    self.select("count(*) as top");
+    self.get(tableName, function (result, err) {
+        if (err)
+            cb(0, err);
+        else
+            cb(result[0].top, err);
+    });
+};
+
+/**
+ * @function limit -
+ * @param {number} limit - limit value.
+ * @param {number} [offset] - offset position. optional value.
+ */
+_db.prototype.limit = function (limit, offset) {
+    var self = this;
+    if (limit !== undefined)
+        self.limitstr = "first " + limit;
+    if (limit !== undefined && offset !== undefined)
+        self.limitstr = " skip " + offset;
+};
+
+/**
  * @function get - get rows from table.
  * @param {string} [tableName] - table name.
  * @param {number} [limit] - limit size. (optional)
@@ -393,14 +648,16 @@ _db.prototype.get = function (tableName, limit, offset, cb) {
     var sql = "";
     if (tableName !== undefined && typeof tableName !== "function") self.fromstr = tableName;
     if (self.driverName === "firebird") {
-        if (limit !== undefined && offset !== undefined)
-            sql = "select first " + limit + " skip " + offset + " " + self.selectstr + " from " + self.fromstr + self.joinstr + self.wherestr + self.groupbystr;
-        else
-            sql = "select " + self.selectstr + " from " + self.fromstr + self.joinstr + self.wherestr + self.groupbystr;
+        if (limit !== undefined && typeof limit !== "function")
+            self.limitstr = "first " + limit;
+        if (limit !== undefined && offset !== undefined && typeof limit !== "function" && typeof offset !== "function")
+            self.limitstr = " skip " + offset;
+        sql = "select " + self.limitstr + " " + self.distinctstr + self.selectstr + " from " + self.fromstr + self.joinstr + self.wherestr + self.groupbystr + self.havingstr + self.orderbystr;
         self.query(sql, undefined, function (result, err) {
+            clearValues(self);
             if (typeof limit === "function")
                 limit(result, err);
-            if (typeof tableName === "function")
+            else if (typeof tableName === "function")
                 tableName(result, err);
             else
                 cb(result, err);
@@ -408,7 +665,7 @@ _db.prototype.get = function (tableName, limit, offset, cb) {
     }
 };
 
-/***
+/**
  * @function init - class constructor
  * @constructs
  * @param {object} framework
