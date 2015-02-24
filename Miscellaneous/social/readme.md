240605b89009fd06ca7541dbde09c77b2401b16f
Peter Širka <petersirka@gmail.com>
2015-02-24 22:15:12 +0100

    new module

---
+++ Miscellaneous/social/readme.md
@@ -0,0 +1,23 @@
+# Login with Facebook, Google, LinkedIn
+
+- copy **social.js** to __/your-totaljs-website/modules/__
+- For testing use: [NGROK - proxy tunnel](https://ngrok.com/)
+- [EXAMPLE](https://github.com/petersirka/total.js-modules/tree/master/social/example)
+
+## Functions and Events
+
+```javascript
+var social = MODULE('social');
+
+// code === controller.query.code
+social.facebook_redirect(key, callbackURL);
+social.facebook_profile(key, secret, code, callbackURL, callback(err, user));
+
+// code === controller.query.code
+social.google_redirect(key, callbackURL);
+social.google_profile(key, secret, code, callbackURL, callback(err, user));
+
+// code === controller.query.code
+social.linkedin_redirect(key, callbackURL);
+social.linkedin_profile(key, secret, code, callbackURL, callback(err, user));
+```
---
+++ Miscellaneous/social/v1.00/social.js
@@ -0,0 +1,101 @@
+// MIT License
+// Copyright Peter Širka <petersirka@gmail.com>
+// Version 1.00
+
+exports.id = 'social';
+exports.version = '1.00';
+
+var stats = { facebook: 0, google: 0,linkedin: 0 };
+
+exports.usage = function() {
+    return stats;
+};
+
+function facebook_redirect(key, url) {
+    return 'https://graph.facebook.com/oauth/authorize?type=web_server&client_id={0}&redirect_uri={1}&scope=email,user_birthday,user_hometown'.format(key, encodeURIComponent(url));
+}
+
+function facebook_profile(key, secret, code, url, callback) {
+    U.request('https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}'.format(key, url, secret, code), ['get'], '', function(err, data, status, headers) {
+
+        if (err)
+            return callback(err, null);
+
+        if (data.indexOf('"error"') !== -1)
+            return callback(JSON.parse(data), null);
+
+        U.request('https://graph.facebook.com/me?' + data, ['get'], '', function(err, data, status) {
+
+            if (err)
+                return callback(err, null);
+
+            var user = JSON.parse(data);
+            user.picture = user.link.replace('www.', 'graph.') + '/picture';
+            stats.facebook++;
+            callback(null, user);
+        });
+    });
+}
+
+function google_redirect(key, url) {
+    return 'https://accounts.google.com/o/oauth2/auth?scope=email%20profile&redirect_uri={0}&response_type=code&client_id={1}'.format(encodeURIComponent(url), key);
+}
+
+function google_profile(key, secret, code, url, callback) {
+    U.request('https://www.googleapis.com/oauth2/v3/token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
+
+        if (err)
+            return callback(err, null);
+
+        if (data.indexOf('"error"') !== -1)
+            return callback(JSON.parse(data), null);
+
+        data = JSON.parse(data);
+
+        U.request('https://www.googleapis.com/plus/v1/people/me', ['get'], '', function(err, data, status) {
+
+            if (err)
+                return callback(err, null);
+
+            var user = JSON.parse(data);
+            stats.google++;
+            callback(null, user);
+
+        }, null, { 'Authorization': 'Bearer ' + data.access_token });
+    });
+}
+
+function linkedin_redirect(key, url) {
+    return 'https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id={0}&redirect_uri={1}&state=987654321'.format(key, encodeURIComponent(url));
+}
+
+function linkedin_profile(key, secret, code, url, callback) {
+    U.request('https://www.linkedin.com/uas/oauth2/accessToken', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
+
+        if (err)
+            return callback(err, null);
+
+        if (data.indexOf('"error"') !== -1)
+            return callback(JSON.parse(data), null);
+
+        data = JSON.parse(data);
+
+        U.request('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,headline,member-url-resources,picture-url,location,public-profile-url,email-address)?format=json', ['get'], '', function(err, data, status) {
+
+            if (err)
+                return callback(err, null);
+
+            var user = JSON.parse(data);
+            stats.linkedin++;
+            callback(null, user);
+
+        }, null, { 'Authorization': 'Bearer ' + data.access_token });
+    });
+}
+
+exports.facebook_redirect = facebook_redirect;
+exports.facebook_profile = facebook_profile;
+exports.google_redirect = google_redirect;
+exports.google_profile = google_profile;
+exports.linkedin_redirect = linkedin_redirect;
+exports.linkedin_profile = linkedin_profile;
