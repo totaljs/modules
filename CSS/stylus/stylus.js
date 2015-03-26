ce7700e99163f26b8e49f768a498da90680b3196
evmek <evmek@users.noreply.github.com>
2015-03-26 11:54:31 +0200

    Create stylus.js

---
+++ CSS/stylus/stylus.js
@@ -0,0 +1,49 @@
+var stylus = require('stylus');
+var fs = require('fs');
+
+exports.install = function(framework) {
+    F.file('STYL', stylus_compiler);
+
+    if (CONFIG('static-accepts').indexOf('.styl') === -1) F.accept('.styl','text/css');
+
+    F.helpers.stylus = function(name, tag) {
+        var self = this;
+        var url = F._routeStatic(name, F.config['static-url-css']);
+        return (tag || true) ? '<link type="text/css" rel="stylesheet" href="' + url + '" />' : url;
+    };
+};
+
+function stylus_compiler(req, res, isValidation) {
+
+    if (isValidation) return req.url.indexOf('.css') !== -1 || req.url.indexOf('.styl') !== -1;
+
+    var self = this;
+
+    var filename = self.path.temp(req.url.replace(/\//g, '-').substring(1));
+
+    if (F.isProcessed(filename)) {
+        self.responseFile(req, res, filename);
+        return;
+    }
+
+    fs.readFile(self.path.public(req.url), function(err, data) {
+
+        if (err) {
+            self.response404(req, res);
+            return;
+        }
+
+        // senkron
+        try {
+            // only compress when RELEASE mode
+            var css = stylus(data.toString('utf8')).set('compress', RELEASE).render();
+        } catch (err) {
+            self.response500(req, res, err);
+            return;
+        }
+
+        fs.writeFileSync(filename, css);
+        self.responseFile(req, res, filename);
+
+    });
+}
