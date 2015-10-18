41f6d67fc6386cecb0fb2e0c4bb8d4135bacb1fb
Peter Širka <petersirka@gmail.com>
2015-10-18 18:00:14 +0200

    Add custom expiration.

--- Miscellaneous/flash/flash.js
+++ Miscellaneous/flash/flash.js
@@ -3,6 +3,7 @@ var flash = {};
 
 exports.version = '2.00';
 exports.id = 'flash';
+exports.expire = 5;
 
 exports.usage = function() {
 	return flash;
@@ -20,13 +21,7 @@ F.middleware('flash', function(req, res, resume, options, controller) {
 	if (!flash[id])
 		flash[id] = { params: {} };
 
-	var expire = 5;
-
-	if (options && options.expire)
-		expire = options.expire || 5;
-
-	flash[id].duration = expire;
-	flash[id].expire = Date.now() + ((1000 * 60) * expire); // 5 minutes
+	flash[id].expire = Date.now() + ((1000 * 60) * exports.expire); // 5 minutes
 	req.$flash = id;
 	resume();
 });
@@ -34,39 +29,41 @@ F.middleware('flash', function(req, res, resume, options, controller) {
 require('http').IncomingMessage.prototype.flash = function(name, value) {
 
 	var item = flash[this.$flash];
-	var params = item.params;
 	var now = Date.now();
 
 	if (name === undefined && value === undefined) {
 		if (item.expire < now)
 			return new Array(0);
-		return params;
+		return item.params;
 	}
 
 	if (value === undefined) {
 		if (item.expire < now)
 			return undefined;
-		return params[name];
+		return item.params[name];
 	}
 
-	item.expire = now + ((1000 * 60) * item.duration);
+	if (item.expire < now)
+		item.params = {};
+
+	item.expire = now + ((1000 * 60) * exports.expire);
 
 	if (value instanceof Array) {
-		if (!params[name]) {
-			params[name] = value;
-			return params;
+		if (!item.params[name]) {
+			item.params[name] = value;
+			return item.params;
 		}
 
-		params[name].push.apply(params[name], value);
-		return params;
+		item.params[name].push.apply(item.params[name], value);
+		return item.params;
 	}
 
-	if (params[name] instanceof Array)
-		params[name].push(value);
+	if (item.params[name] instanceof Array)
+		item.params[name].push(value);
 	else
-		params[name] = [value];
+		item.params[name] = [value];
 
-	return params;
+	return item.params;
 };
 
 // Extend controller
