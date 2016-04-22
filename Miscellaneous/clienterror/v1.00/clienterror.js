32c5f3a59ca39db8a68f5fd19868c5b7c6f93349
Peter Širka <petersirka@gmail.com>
2016-04-22 06:58:05 +0200

    Improved module.

--- Miscellaneous/clienterror/v1.00/clienterror.js
+++ Miscellaneous/clienterror/v1.00/clienterror.js
@@ -1,92 +1,50 @@
 // MIT License
 // Copyright Peter Širka <petersirka@gmail.com>
 
-var events = require('events');
+const Events = require('events');
+const REGEXP_BROWSER = /Chrome\/\d+|Firefox\/[0-9.]|Safari\/\d+|MSIE.\d+|Opera\/[0-9.]+/;
 var stats_errors = 0;
 
-function ClientError () {}
-ClientError.prototype.__proto__ = new events.EventEmitter();
-
-var clienterror = new ClientError();
-
+module.exports = new Events.EventEmitter();
 module.exports.name = 'clienterror';
-module.exports.version = '2.00';
-module.exports.on = (name, callback) => clienterror.on(name, callback);
-module.exports.instance = clienterror;
-module.exports.usage = function() {
-    return {
-        errors: stats_errors
-    };
-};
+module.exports.usage = () => { errors: stats_errors };
 
-exports.install = function() {
-
-    // Backward compatibility
-    var options = F.version >= 1900 ? arguments[0] : arguments[1];
-
-    clienterror.options = Utils.extend({ logger: true, console: false, filename: 'clienterror' }, options, true);
-
-    F.on('controller', onController);
-    F.route('/$clienterror/', json_error, ['post', 'json', 'referer']);
+module.exports.install = function(options) {
+	clienterror.options = U.extend({ logger: true, console: false, filename: 'clienterror' }, options, true);
+	F.on('controller', onController);
+	F.route('/$clienterror/', process_error, ['post', 'json', 'referer']);
 };
 
 module.exports.uninstall = function() {
-    F.removeListener('controller', onController);
-    clienterror = null;
+	F.removeListener('controller', onController);
+	module.exports.removeAllListeners();
 };
 
 function onController(controller) {
-    controller.head("<script>window.onerror=function(e){var err=e.toString();if(window.CLIENTERROR===err)return;window.CLIENTERROR=err;var xhr=new XMLHttpRequest();xhr.open('POST','/$clienterror/',true);xhr.setRequestHeader('Content-type','application/json');xhr.send(JSON.stringify({url:window.location.href,error:e}));};</script>");
+	controller.head("<script>window.onerror=function(e){var err=e.toString();if(window.CLIENTERROR===err)return;window.CLIENTERROR=err;var xhr=new XMLHttpRequest();xhr.open('POST','/$clienterror/',true);xhr.setRequestHeader('Content-type','application/json');xhr.send(JSON.stringify({url:location.href,error:e}));};</script>");
 }
 
-function json_error() {
-    var self = this;
-    var body = self.body;
+function process_error() {
+	var self = this;
+	var body = self.body;
+	var ua = self.req.headers['user-agent'] || '';
+	var browser = '';
 
-    console.log('error')
+	self.plain('');
+	stats_errors++;
 
-    self.plain('');
+	var r = ua.match(REGEXP_BROWSER);
+	if (r)
+		browser = r.toString();
 
-    var ua = self.req.headers['user-agent'] || '';
-    var browser = '';
+	if ((/mobile/i).test(ua))
+		browser += ' (mobile)';
 
-    stats_errors++;
+	if (clienterror.options.logger)
+		self.logger(clienterror.options.filename, body.url, body.error, browser);
 
-    var r = ua.match(/Chrome\/\d+/);
-    if (r)
-        browser = r.toString();
-    else {
-        r = ua.match(/Firefox\/[0-9.]+/);
-        if (r)
-            browser = r.toString();
-        else {
-            r = ua.match(/Safari\/\d+/);
-            if (r)
-                browser = r.toString();
-            else {
-                r = ua.match(/MSIE.\d+/);
-                if (r)
-                    browser = r.toString();
-                else {
-                    r = ua.match(/Opera\/[0-9.]+/);
-                    if (r)
-                        browser = r.toString();
-                    else {
-                        browser = ua;
-                    }
-                }
-            }
-        }
-    }
+	if (clienterror.options.console)
+		console.log('CLIENTERROR:', body.url, body.error, browser);
 
-    if ((/mobile/i).test(ua))
-        browser += ' (mobile)';
-
-    if (clienterror.options.logger)
-        self.logger(clienterror.options.filename, body.url, body.error, browser);
-
-    if (clienterror.options.console)
-        console.log('CLIENTERROR:', body.url, body.error, browser);
-
-    clienterror.emit('clienterror', {url: body.url, error: body.error, browser: browser});
+	module.exports.emit('clienterror', { url: body.url, error: body.error, browser: browser });
 }
