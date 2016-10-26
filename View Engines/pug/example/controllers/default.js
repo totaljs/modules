d07bc75989eb2dd168d194f4def36e0939401a93
Peter Širka <petersirka@gmail.com>
2016-10-26 07:38:27 +0200

    Add PUG view engine.

---
+++ View Engines/pug/example/controllers/default.js
@@ -0,0 +1,11 @@
+exports.install = function() {
+	F.route('/', view_homepage);
+};
+
+function view_homepage() {
+	var self = this;
+	var tobi = { name: 'tobi', age: 2 };
+	var loki = { name: 'loki', age: 1 };
+	var jane = { name: 'jane', age: 5 };
+	self.view('extend', { title: 'pets', pets: [tobi, loki, jane] });
+}
