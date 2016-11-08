799b70c58ccae10ed37a748307c3e91f8814bd51
Peter Širka <petersirka@gmail.com>
2016-11-08 12:53:29 +0100

    Update `clienterror` module (improve error message).

--- Miscellaneous/clienterror/clienterror.js
+++ Miscellaneous/clienterror/clienterror.js
@@ -21,7 +21,7 @@ module.exports.uninstall = function() {
 };
 
 function onController(controller) {
-	!controller.robot && controller.head("<script>window.onerror=function(e){var err=e.toString();if(window.CLIENTERROR===err)return;window.CLIENTERROR=err;var x=new XMLHttpRequest();x.open('POST','{0}',true);x.setRequestHeader('Content-type','application/json');x.send(JSON.stringify({url:location.href,error:e}));};</script>".format(options.url));
+	!controller.robot && controller.head("<script>window.onerror=function(e){var err=e.stack.toString();if(window.CLIENTERROR===err)return;window.CLIENTERROR=err;var x=new XMLHttpRequest();x.open('POST','{0}',true);x.setRequestHeader('Content-type','application/json');x.send(JSON.stringify({url:location.href,error:e}));};</script>".format(options.url));
 }
 
 function process_error() {
