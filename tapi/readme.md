b6e40d4ea54618fefb68a8e18309e3e54949e5ce
Peter Širka <petersirka@gmail.com>
2023-03-13 16:11:57 +0100

    Added new module.

---
+++ tapi/readme.md
@@ -0,0 +1,7 @@
+# TAPI
+
+This module will list availiable Total.js API endpoints. The endpoint can be used in the Total.js Flow directly in the TAPI component.
+
+## Configuration
+
+- `CONF.tapiurl {String}` can change API meta endpoint (default: `/api/`)
---
+++ tapi/tapi.js
@@ -0,0 +1,48 @@
+exports.install = function() {
+	ROUTE('GET ' + (CONF.tapiurl || '/api/'), api);
+};
+
+function api() {
+
+	var $ = this;
+
+	if (TEMP.TAPI) {
+		$.json(TEMP.TAPI);
+		return;
+	}
+
+	var items = [];
+	var output = [];
+
+	EACHSCHEMA(function(name, schema) {
+
+		for (var key in schema.actions) {
+			var action = schema.actions[key];
+			if (action.public)
+				items.push({ action: key, schema: name, icon: action.icon, name: action.name, params: action.params, input: action.input, ouptut: action.ouptut, query: action.query, summary: action.summary });
+		}
+
+	});
+
+	for (var a in F.routes.api) {
+		var actions = F.routes.api[a];
+		for (var b in actions) {
+			var action = actions[b];
+			for (var i = 0; i < items.length; i++) {
+				var m = items[i];
+				if (action.action.indexOf(' ' + m.action) !== -1 && action.action.indexOf(m.schema + ' ') !== -1) {
+					items.splice(i, 1);
+					m.id = action.name;
+					m.url = action.url;
+					m.action = undefined;
+					m.schema = undefined;
+					output.push(m);
+					break;
+				}
+			}
+		}
+	}
+
+	TEMP.TAPI = output;
+	$.json(output);
+}
